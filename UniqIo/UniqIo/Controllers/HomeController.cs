bb29a6a5fe415a82813c3bbcaea164aab34d6539
MuradCavadov7 <muradnc-ap117@code.edu.az>
2024-11-21 00:03:30 +0400

    FirstStep UniqIo

---
+++ UniqIo/UniqIo/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+//using UniqIo.Models;
+
+namespace UniqIo.Controllers
+{
+	public class HomeController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult About()
+		{
+			return View();
+		}
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+    }
+}
